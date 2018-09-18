ed9bc665757410c0787179527ef34daebbfa7a53
jmora0324 <42849299+jmora0324@users.noreply.github.com>
2018-09-17 19:23:43 -0700

    commit

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -96,7 +96,7 @@ $$(document).on('pageInit', '.page[data-page="map"]', function (e) {
       // If you click the button, do something...
       var button = document.getElementById("button");
       button.addEventListener("click", function() {
-alert (map.length);
+//alert (map.length);
         // Move to the position with animation
         map.animateCamera({
           target: {lat: 37.422359, lng: -122.084344},
